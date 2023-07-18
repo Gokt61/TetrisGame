7306cf1b367627b234b4e8e91767e93ea1ddaba1
Gokt61 <49290553+Gokt61@users.noreply.github.com>
2023-07-18 18:43:51 +0300

    ShapeManager scriptinde Hareket kodları yazıldı

--- Tetris/Assets/Scripts/ShapeManager.cs
+++ Tetris/Assets/Scripts/ShapeManager.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class ShapeManager : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private bool donebilirmi = true;
+
+    private void Start()
     {
-        
+        //InvokeRepeating("AssagiHareketFNC",0f,0.25f);
+        StartCoroutine(HareketRoutine());
     }
 
-    void Update()
+    public void SolaHareketetFNC()
     {
-        
+        transform.Translate(Vector3.left);
+    }
+    public void SagaHareketetFNC()
+    {
+        transform.Translate(Vector3.right);
+    }
+
+    public void AssagiHareketFNC()
+    {
+        transform.Translate(Vector3.down);
+    }
+    public void YukariHareketFNC()
+    {
+        transform.Translate(Vector3.up);
+    }
+
+    public void SagaDonFNC()
+    {
+        if (donebilirmi)
+        {
+            transform.Rotate(0, 0, -90);
+        }
+    }
+    public void SolaDonFNC()
+    {
+        if (donebilirmi)
+        {
+            transform.Rotate(0, 0, 90);
+        }
+    }
+
+    IEnumerator HareketRoutine()
+    {
+        while (true)
+        {
+            AssagiHareketFNC();
+            yield return new WaitForSeconds(0.25f);
+        }
     }
 }
