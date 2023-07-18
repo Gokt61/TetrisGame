43474490e6860e207b3bd7df522f0d5514939245
Gokt61 <49290553+Gokt61@users.noreply.github.com>
2023-07-18 16:17:48 +0300

    BoardManager eklendi

---
+++ Tetris/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    [SerializeField] private Transform tilePrefab;
+
+    public int yukseklik = 22;
+    public int genislik = 10;
+
+    private void Start()
+    {
+        BosKareleriOlusturFNC();
+    }
+
+    void BosKareleriOlusturFNC()
+    {
+        if (tilePrefab != null)
+        {
+            for (int y = 0; y < yukseklik; y++)
+            {
+                for (int x = 0; x < genislik; x++)
+                {
+                    Transform tile = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                    tile.name = "x " + x.ToString() + " ," + y.ToString();
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Tile Prefab eklenmedi");
+        }
+    }
+}
